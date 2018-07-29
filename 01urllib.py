9994b2ca24358762360c538dd6424ce23cfced5c
hexin <hexinbq@163.com>
2018-07-29 14:22:20 +0800

    change 01urllib.py

--- 01urllib.py
+++ 01urllib.py
@@ -7,7 +7,7 @@ response = urllib2.urlopen(url)
 html = response.read()
 release_list = re.findall(r'<a href="[^.].*?">(.*?/)</a>',html)
 print(release_list)
-print("origin")
+print("don't want to change")
 
 print('---------------ok--')
 for i in xrange(0,len(release_list)):
