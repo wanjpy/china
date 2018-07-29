c072f38290bf93078420086257a887e118543114
hexin <hexinbq@163.com>
2018-07-29 09:27:05 +0800

    print subdir any dir.

--- 01urlib.py
+++ 01urlib.py
@@ -5,9 +5,12 @@ import re
 url = "http://mirrors.163.com/debian-cd/"
 response = urllib2.urlopen(url)
 html = response.read()
-
-release_list = re.findall(r'<a href=".*?">(.*?)</a>',html)
+release_list = re.findall(r'<a href="[^.].*?">(.*?/)</a>',html)
 print(release_list)
 
-response2 = urllib2.urlopen(url+release_list[1])
-print(response2.read())
+for i in xrange(0,len(release_list)):
+    html2 = urllib2.urlopen(url+release_list[i]).read()
+    sub_list = re.findall(r'<a href="[^.].*?">(.*?/)</a>', html2)
+    print(sub_list)
+    print('--------------')
+
